5344b624b635faf77c2236b2c3494c9e1a013394
Juliano Costa Silva <apfjuliano@gmail.com>
2020-12-23 14:52:19 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9b1fa35c5237f48ee2a398415bf2ae03'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e5505310-1d8a-407c-b409-6c3f927fc498'
+};
